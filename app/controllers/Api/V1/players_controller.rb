3c9e74c8be8324b51f3d38ea456bb4dd8e7bf18a
Denis Lafontant <icebox827@gmail.com>
2022-08-25 19:17:40 -0400

    Update app

--- app/controllers/Api/V1/players_controller.rb
+++ app/controllers/Api/V1/players_controller.rb
@@ -1,8 +1,13 @@
 module Api 
   module V1
     class PlayersController < ApplicationController
-      def index; end
+      def index
+        @players = player.all
+      end
+
+      def show
+        @player = PlayerList::Import.find(params[:id])
+      end
     end
   end
 end
-
--- lib/import_data.rb
+++ lib/import_data.rb
@@ -1,31 +1,31 @@
 require 'json'
 require 'uri'
 
-class Import
-  def call
-    File.foreach(Rails.root.join('tmp/player_list.json')) do |player_list|
-      player_hash = JSON.parse(player_list)
-      create_player(player_hash)
+module PlayerList
+  class Import
+    def call
+      IO.foreach(Rails.root.join('tmp/player_list.json')) do |player_list|
+        player_hash = JSON.parse(player_list)
+        create_player(player_hash)
+      end
     end
-  end
 
-  private
+    private
 
-  def create_player(player_hash)
-    player = Player.new
-    player.id = player_hash.fetch('id')
-    player.name_brief = player_hash.fetch('fullname')
-    player.first_name = player_hash.fetch('firstname')
-    player.last_name = player_hash.fetch('lastname')
-    player.position = player_hash.fetch('position')
-    player.age = player_hash.fetch('age')
-    player.average_position_age_diff = player_hash.fetch('age')
-    if player.save
-      "player #{player_hash['id']}created"
-    else
-      "player #{player_hash['id']} was not created"
+    def create_player(player_hash)
+      player = Player.new
+      player.uuid = player_hash.fetch('id')
+      player.first_name = player_hash.fetch('firstname')
+      player.last_name = player_hash.fetch('lastname')
+      player.position = player_hash.fetch('position')
+      player.age = player_hash.fetch('age')
+
+
+      if player.save
+        "player #{player_hash['id']}created"
+      else
+        "player #{player_hash['id']} was not created"
+      end
     end
   end
 end
-
-Import.new.call
