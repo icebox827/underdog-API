77e0864ffc3963fdecc03b010948a6a7e5616aa0
Denis Lafontant <icebox827@gmail.com>
2022-08-26 17:27:31 -0400

    Get data

--- app/controllers/Api/V1/players_controller.rb
+++ app/controllers/Api/V1/players_controller.rb
@@ -1,13 +1,27 @@
-module Api 
+module Api
   module V1
     class PlayersController < ApplicationController
+      require 'rest-client'
+
       def index
-        @players = player.all
+        url = 'http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=basketball&response_format=JSON'
+        players = RestClient.get(url)
+        render json: players, status: :ok
       end
 
-      def show
-        @player = PlayerList::Import.find(params[:id])
-      end
+      # def show
+      #   url = `http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT={sport}&response_format=JSON`
+      #   player = RestClient.get(url, {params: {id:}})
+      #   render json: player
+      # end
+
+      # def create 
+      #   player.uuid = player_hash.fetch('id')
+      #   player.first_name = player_hash.fetch('firstname')
+      #   player.last_name = player_hash.fetch('lastname')
+      #   player.position = player_hash.fetch('position')
+      #   player.age = player_hash.fetch('age')
+      # end
     end
   end
 end
