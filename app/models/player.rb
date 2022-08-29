f0dc76d8a8bd8c489cd2b4289cd3040285c766c4
Denis Lafontant <icebox827@gmail.com>
2022-08-29 08:36:39 -0400

    Update player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
-
+  validates :first_name, :last_name, presence: true, length: { in: 2..25 }
+  validates :position, :age, presence: true
 end
--- lib/import_data.rb
+++ lib/import_data.rb
@@ -1,31 +1,28 @@
 require 'json'
 require 'uri'
 
-module PlayerList
-  class Import
-    def call
-      IO.foreach(Rails.root.join('tmp/player_list.json')) do |player_list|
-        player_hash = JSON.parse(player_list)
-        create_player(player_hash)
-      end
-    end
+class Import
+  def get_data
+    url = 'http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=basketball&response_format=JSON'
+    players = RestClient.get(url)
+    player_hash = JSON.parse(players)
+    create_player(player_hash)
+  end
 
-    private
+  private
 
-    def create_player(player_hash)
-      player = Player.new
-      player.uuid = player_hash.fetch('id')
-      player.first_name = player_hash.fetch('firstname')
-      player.last_name = player_hash.fetch('lastname')
-      player.position = player_hash.fetch('position')
-      player.age = player_hash.fetch('age')
+  def create_player(player_hash)
+    player = Player.new
+    player.uuid = player_hash.fetch('id')
+    player.first_name = player_hash.fetch('firstname')
+    player.last_name = player_hash.fetch('lastname')
+    player.position = player_hash.fetch('position')
+    player.age = player_hash.fetch('age')
 
-
-      if player.save
-        "player #{player_hash['id']}created"
-      else
-        "player #{player_hash['id']} was not created"
-      end
+    if player.save
+      "player created sucessfully"
+    else
+      "player was not created"
     end
   end
 end
